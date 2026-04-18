b5c92172eede591e68481580164fd2f2f1bd9d97
anhenrique <anhenrique1@gmail.com>
2026-04-18 20:04:01 -0300

    fix outputs

--- output.tf
+++ output.tf
@@ -2,15 +2,10 @@ output "vpc_id"{
     value = aws_vpc.main.id
 }
 
-output "aws_instance" {
+output "aws_instance_public_ip"{
     value = aws_instance.lab.public_ip
 }
-output "aws_instance" {
-    value = aws_instance.lab.get_password_data
-}
-output "aws_instance" {
+
+output "aws_instance_public_dns" {
     value = aws_instance.lab.public_dns
-}
-output "aws_instance" {
-    value = aws_instance.lab.public_ip
 }
